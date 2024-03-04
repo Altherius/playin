de148a163f40dade4aea70052516bfc1b872ac14
Maxime Eychenne <maxime.eychenne@gmail.com>
2024-03-04 14:03:26 +0100

    feat: implement stock listener to update available quantity

---
+++ app/Listeners/StockValidatedListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderValidated;
+use App\Events\StockValidated;
+use App\Models\ProductLocalProperties;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
+
+class StockValidatedListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(StockValidated $event): void
+    {
+        if ($event->stock->getAttributes() && !$event->stock->getOriginal()['validated']) {
+            foreach ($event->stock->items as $item) {
+                $localProperties = ProductLocalProperties::firstOrCreate([
+                    'product_id' => $item->product_id,
+                    'store_id' => $event->stock->store_id
+                ]);
+
+                $localProperties->available_quantity += $item->quantity;
+                $localProperties->save();
+            }
+        }
+    }
+}
