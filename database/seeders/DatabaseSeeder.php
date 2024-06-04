79980e19def8251ed07a462a94b703826ec74647
Maxime Eychenne <maxime.eychenne@gmail.com>
2024-06-04 14:51:11 +0200

    refactor: use recycling for database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,16 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->has(Address::factory()->count(2))->create();
+        $users = \App\Models\User::factory(10)->has(Address::factory()->count(2))->create();
+        $stores = \App\Models\Store::factory(3)->create();
         \App\Models\Product::factory(10)->create();
         \App\Models\Product::factory(10)->boardgame()->create();
         \App\Models\Product::factory(10)->magic()->create();
         \App\Models\Product::factory(10)->yugioh()->create();
         \App\Models\Product::factory(10)->fab()->create();
         \App\Models\Product::factory(10)->lorcana()->create();
-        \App\Models\Store::factory(3)->create();
-        \App\Models\Order::factory(5)->has(Address::factory())->create();
-        \App\Models\Stock::factory(5)->has(Address::factory())->create();
+        \App\Models\Order::factory(5)->has(Address::factory())->recycle($users)->recycle($stores)->create();
+        \App\Models\Stock::factory(5)->has(Address::factory())->recycle($users)->recycle($stores)->create();
         \App\Models\BoardgameProperties::factory(5)->create();
         \App\Models\CardPropertiesMagic::factory(1)->create();
         \App\Models\CardPropertiesYugioh::factory(1)->create();
@@ -30,6 +30,6 @@ class DatabaseSeeder extends Seeder
         \App\Models\CardPrintState::factory(1)->create();
         \App\Models\CardEdition::factory(10)->create();
         \App\Models\CardRelease::factory(1)->create();
-        \App\Models\Event::factory(10)->create();
+        \App\Models\Event::factory(10)->recycle($stores)->create();
     }
 }
